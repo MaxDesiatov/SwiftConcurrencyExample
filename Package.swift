ff1d1dd8765bf173618b3569caea94938041c016
Max Desiatov <max@desiatov.com>
2021-01-09 17:26:55 +0000

    Update package name in Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "ConcurrencyPlayground",
+    name: "ConcurrencyExample",
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
@@ -13,7 +13,7 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "ConcurrencyPlayground",
+            name: "ConcurrencyExample",
             dependencies: [],
             swiftSettings: [
                 .unsafeFlags(["-Xfrontend", "-enable-experimental-concurrency"]),
