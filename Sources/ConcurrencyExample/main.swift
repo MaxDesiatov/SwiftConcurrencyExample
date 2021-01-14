63a18177dca1ace707999d15eb737d1955dd4d50
Max Desiatov <max@desiatov.com>
2021-01-14 13:33:12 +0000

    Link to the article from `README.md`

--- Sources/ConcurrencyExample/main.swift
+++ Sources/ConcurrencyExample/main.swift
@@ -6,12 +6,14 @@
 //
 
 import _Concurrency
+import Dispatch
 import Foundation
 
 struct UnknownError: Error {}
 
 func download(_ url: String) async throws -> Data {
-    try await withUnsafeThrowingContinuation { c in
+    print("fetching \(url)")
+    return try await withUnsafeThrowingContinuation { c in
         let task = URLSession.shared.dataTask(with: URL(string: url)!) { data, _, error in
             switch (data, error) {
             case let (_, error?):
@@ -26,19 +28,53 @@ func download(_ url: String) async throws -> Data {
     }
 }
 
+func sleep(seconds: Int) async {
+    await withUnsafeContinuation { c in
+        DispatchQueue.global().asyncAfter(deadline: .now() + .seconds(seconds)) {
+            c.resume(returning: ())
+        }
+    }
+}
+
+func childTasks() async throws -> String {
+    print("\(#function) started")
+
+    async let uuid1 = download("https://httpbin.org/uuid")
+    async let uuid2 = download("https://httpbin.org/uuid")
+    try await Task.checkCancellation()
+    print("\(#function) is not cancelled yet")
+
+    return try await """
+    ids fetched conncurrently:
+    \(String(data: uuid1, encoding: .utf8)!)\(String(data: uuid2, encoding: .utf8)!)
+    """
+}
+
 runAsyncAndBlock {
     print("task started")
     let data = try! await download("https://httpbin.org/uuid")
     print(String(data: data, encoding: .utf8)!)
 
-    async let uuid1 = download("https://httpbin.org/uuid")
-    async let uuid2 = download("https://httpbin.org/uuid")
+    try! await print(childTasks())
 
+    // run this as a detached task and get a handle for it
+    let handle = Task.runDetached {
+        await sleep(seconds: 1)
+        try await print(childTasks())
+    }
 
-    try! await print("""
-    ids fetched conncurrently:
-    \(String(data: uuid1, encoding: .utf8)!)\(String(data: uuid2, encoding: .utf8)!)
-    """)
+    // cancel the task immediately through the handle
+    handle.cancel()
+
+    do {
+        try await handle.get()
+    } catch {
+        if error is Task.CancellationError {
+            print("task cancelled")
+        } else {
+            print(error)
+        }
+    }
 }
 
 print("end of main")
